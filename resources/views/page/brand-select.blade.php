4f36fe9f2159b73b19f7711478fe9571a70e9006
offcwho <nhnhnhbgbg@gmail.com>
2025-02-01 14:59:26 +0300

    upd

--- resources/views/page/brand-select.blade.php
+++ resources/views/page/brand-select.blade.php
@@ -1,5 +1,5 @@
 <x-app.layout>
     <x-slot:title>{{ $titlePage }}</x-slot:title>
-    <h1>Бренд</h1>
+    <h1>Брендs</h1>
     <livewire:product.product-list :products="$products" />
 </x-app.layout>
